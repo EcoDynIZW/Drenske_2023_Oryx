587456d798d0bcca44b2320d6cb4c4d174778cdc
SinahDre <sinah.drenske@campus.tu-berlin.de>
2020-11-25 10:29:35 +0100

    Initialise project
    Adding or updating files for the start of the project

--- R/00_start.R
+++ R/00_start.R
@@ -2,12 +2,12 @@
 
 ## Add meta data about your project to DESCRIPTION
 d6::fill_desc(
-  pkg_title = "",         ## The Full Title of the Project
-  pkg_description = "",   ## The Description of Your Project
-  author_first_name = "", ## Your First Name
-  author_last_name = "",  ## Your Last Name
-  author_email = "",      ## Your Email
-  repo_url = NULL         ## The URL of the GitHub Repo (optional)
+  pkg_title = "PVA of the Northern Bald Ibis",         ## The Full Title of the Project
+  pkg_description = "Analysis of the survival, fecundity and population viability of reintroduced Northern Bald Ibises in Germany and Austria",   ## The Description of Your Project
+  author_first_name = "Sinah", ## Your First Name
+  author_last_name = "Drenske",  ## Your Last Name
+  author_email = "sinah.drenske@campus.tu-berlin.de",      ## Your Email
+  repo_url = "https://github.com/SinahDre/Drenske_2020_PVA_NBI"         ## The URL of the GitHub Repo (optional)
 )
 
 ## Add and fill the readme
@@ -15,9 +15,10 @@ usethis::use_readme_md()
 
 ## Add license if needed
 ## See ?usethis::use_mit_license for more information
-usethis::use_mit_license( name = "Your Name" )
+usethis::use_mit_license( name = "Sinah Drenske" )
+# Name = name of the copyright holder(s)
 
 ## ADD PACKAGE DEPENDENCIES ----
 
 ## Add one line by package you want to add as dependency
-usethis::use_package("tidyverse")
+usethis::use_package("tidyverse", type = "depends")
---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Drenske_2020_PVA_NBI
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of Drenske_2020_PVA_NBI is to analyse the survival and fecundity probabilities of the Northern Bald Ibis (NBI). Subsequently, these results are used to perform a population viability analysis in NetLogo and R. 
+
+## Installation
+
+<!-- You can install the released version of Drenske_2020_PVA_NBI from [CRAN](https://CRAN.R-project.org) with:-->
+You can install the released version of Drenske_2020_PVA_NBI from [GitHub](https://github.com/) with:
+
+``` r
+devtools::install_github("SinahDre/Drenske_2020_PVA_NBI")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(Drenske_2020_PVA_NBI)
+## basic example code
+```
+
